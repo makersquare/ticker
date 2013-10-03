90645b40144162d7cb46cf268202a4f236025a1c
Matt Buck <github@matthewbuck.com>
2013-10-03 18:44:56 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticker::Application.config.secret_key_base = 'b0597d25973464c9f6ca262404408eb580f98c6aabd1e6ffb0fbc2c5873301809a746a7d070365fd69fb1e43dd62999c255bcf1ec0306785d17e5ec4a6e6e738'
