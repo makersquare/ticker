39b2c017c90bea010acc5bc5b0210e21b04566f2
Matt Buck <github@matthewbuck.com>
2013-10-03 18:56:01 -0500

    Build worker process

---
+++ app/workers/quote_worker.rb
@@ -0,0 +1,13 @@
+class QuoteWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  recurrence { secondly(5) }
+
+  def perform
+    stock_url = "http://query.yahooapis.com/v1/public/yql?q=select%20symbol%2C%20AskRealtime%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=updateStocks"
+    resp = RestClient.get(stock_url)
+
+    Rails.cache.write 'stock_api_call', resp
+  end
+end
