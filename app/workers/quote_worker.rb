f259ab22faf4f30db42c0e2ebe67c82868268722
Matt Buck <github@matthewbuck.com>
2013-10-03 19:43:09 -0500

    Dial back the number of requests

--- app/workers/quote_worker.rb
+++ app/workers/quote_worker.rb
@@ -2,7 +2,7 @@ class QuoteWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { secondly }
+  recurrence { secondly(3) }
 
   def perform
     stock_url = "http://query.yahooapis.com/v1/public/yql?q=select%20symbol%2C%20AskRealtime%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=updateStocks"
